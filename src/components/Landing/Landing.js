4caef48832347e2ab78bc786e3d4c484d50ad58a
Philip Schultz <24783715+schupp13@users.noreply.github.com>
2019-10-22 12:12:20 -0400

    still need to make page marks

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import "./Landing.scss";
+import Projects from "../Projects/Projects";
 import Me3 from "../../pics/me3.png";
-import { tsConstructorType } from "@babel/types";
+import phone from "../../pics/phone.png";
+import email from "../../pics/email.png";
 
 export default class Landing extends Component {
   constructor() {
@@ -26,12 +28,33 @@ export default class Landing extends Component {
           <div className="content">
             <div className="title">
               <h1>Philip Schultz Portfolio</h1>
-
               <h3>Full Stack Web Developer</h3>
+              {this.state.classContact === "hello" && (
+                <div className="bitmoji">
+                  <img src={Me3} />
+                </div>
+              )}
+              {this.state.classContact === "phone" && (
+                <div className="bitmoji">
+                  <img src={phone} />
+                  <a href="tel:19047043965">
+                    <h2>904-704-3965</h2>
+                  </a>
+                </div>
+              )}
+              {this.state.classContact === "email" && (
+                <div className="bitmoji">
+                  <img src={email} />
+                  <a href="mailto:webmaster@example.com">
+                    <h2>schupp@protonmail.com</h2>
+                  </a>
+                </div>
+              )}
             </div>
-            <div className={this.state.classContact}></div>
             <div className="contact">
-              <i class="fab fa-github"></i>
+              <a href="https://github.com/schupp13" target="_blank">
+                <i class="fab fa-github"></i>
+              </a>
               <i
                 class="fas fa-envelope"
                 onClick={() => this.phoneClass("email")}
@@ -41,7 +64,12 @@ export default class Landing extends Component {
                 class="fas fa-phone-volume"
                 onClick={() => this.phoneClass("phone")}
               ></i>
-              <i class="fab fa-linkedin"></i>
+              <a
+                href="https://www.linkedin.com/in/philip-schultz-bb3742125/"
+                target="_blank"
+              >
+                <i class="fab fa-linkedin"></i>
+              </a>
             </div>
             <div className="bio">
               <p>
@@ -53,16 +81,9 @@ export default class Landing extends Component {
                 Transitioning from a career in customer service/ sales.{" "}
               </p>
             </div>
-            <div className="skills">
-              <i class="fab fa-js-square"></i>
-              <i class="fab fa-html5"></i>
-              <i class="fab fa-css3-alt"></i>
-              <i class="fab fa-sass"></i>
-              <i class="fab fa-react"></i>
-              <i class="fab fa-node"></i>
-              <i class="fas fa-database"></i>
-            </div>
           </div>
+
+          <Projects />
         </div>
       </div>
     );
